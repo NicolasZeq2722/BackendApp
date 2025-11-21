44eab88f77151fb72e543ae8ff5b017c613a89c8
NicolasZeq2722 <nicomansolov89@gmail.com>
2025-11-21 17:16:09 -0500

    Nuevos cambios frontend

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import UsersScreen from './src/screens/UsersScreen';
+import ProductsScreen from './src/screens/ProductsScreen';
+import CategoriesScreen from './src/screens/CategoriesScreen';
+import SucategoriesScreen from './src/screens/SubcategoriesScreen';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{title: "Menu principal"}}/>
+        <Stack.Screen name="Users" component={UsersScreen} options={{title: "Gestion de usuarios"}}/>
+        <Stack.Screen name="Products" component={ProductsScreen} options={{title: "Productos"}}/>
+        <Stack.Screen name="Categories" component={CategoriesScreen} options={{title: "Categorías"}}/>
+        <Stack.Screen name="Subcategories" component={SucategoriesScreen} options={{title: "Subcategorías"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
